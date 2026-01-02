e8857ce3c8abebecde595f99a77f0437c514b75c
mails4dine <mails4dine@gmail.com>
2026-01-02 05:52:00 +0530

    Resolved IOException: Used Absolute Path and Flush delay for Report auto-open

--- src/main/java/genericUtility/ListenerImplementation.java
+++ src/main/java/genericUtility/ListenerImplementation.java
@@ -12,43 +12,54 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ListenerImplementation implements ITestListener {
 
     public ExtentReports report;
     public static ThreadLocal<ExtentTest> testThread = new ThreadLocal<>(); 
-    WebDriverUtility wUtil = new WebDriverUtility();
+    // This variable will hold the absolute path
+    public static String absolutePath;
 
-        // 1. Declare a global string to hold the report location
-        String reportFullName;
+    @Override
+    public void onStart(ITestContext context) {
+        // Use a simpler date format to avoid OS character issues
+        String time = new Date().toString().replace(" ", "_").replace(":", "_");
+        
+        // Define the path clearly
+        String fileName = "Report_" + time + ".html";
+        File directory = new File("./ExtentReports/");
+        if (!directory.exists()) directory.mkdirs();
+        
+        // Create an absolute path string
+        File reportFile = new File(directory, fileName);
+        absolutePath = reportFile.getAbsolutePath();
+        
+        ExtentSparkReporter spark = new ExtentSparkReporter(absolutePath);
+        spark.config().setDocumentTitle("Ninza CRM Execution Report");
+        spark.config().setReportName("Regression Suite");
+        spark.config().setTheme(Theme.DARK);
 
-        @Override
-        public void onStart(ITestContext context) {
-            String time = new Date().toString().replace(" ", "_").replace(":", "_");
-            
-            // 2. Assign the path here once
-            reportFullName = "./ExtentReports/Report_" + time + ".html";
-            
-            ExtentSparkReporter spark = new ExtentSparkReporter(reportFullName);
-            spark.config().setDocumentTitle("Ninza CRM Execution Report");
-            // ... rest of your config
-            
-            report = new ExtentReports();
-            report.attachReporter(spark);
-        }
+        report = new ExtentReports();
+        report.attachReporter(spark);
+    }
 
-        @Override
-        public void onFinish(ITestContext context) {
-            report.flush();
-            
-            // 3. Use that same variable to open the file
-            File file = new File(reportFullName);
-            try {
+    @Override
+    public void onFinish(ITestContext context) {
+        report.flush(); 
+        
+        // Use a small delay to ensure the OS has finished writing the file
+        try {
+            Thread.sleep(2000); 
+            File file = new File(absolutePath);
+            if (file.exists()) {
                 Desktop.getDesktop().browse(file.toURI());
-            } catch (IOException e) {
-                e.printStackTrace();
+            } else {
+                System.out.println("Wait... file still not found at: " + absolutePath);
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    
+    }
+    // ... other methods
 }
